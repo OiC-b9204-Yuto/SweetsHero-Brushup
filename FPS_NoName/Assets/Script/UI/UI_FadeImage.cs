ddf10b76d1b72328e55c4633d7f137aab5d0ac15
DESKTOP-G23FMHQ\senz <ryuuki285666@gmail.com>
2022-01-17 05:05:21 +0900

    メインメニューの切り替え時の演出を追加 --> まだ未完成

--- FPS_NoName/Assets/Script/UI/UI_FadeImage.cs
+++ FPS_NoName/Assets/Script/UI/UI_FadeImage.cs
@@ -8,12 +8,21 @@ public class UI_FadeImage : MonoBehaviour
     [SerializeField] private float FadeTime;
     [SerializeField] private bool FadeOut;
     [SerializeField] private bool FadeIn;
+    [SerializeField] private bool IsTimingFadeImage;
+    [SerializeField] private bool SpeedUpFadeSpeedx2;
+    public bool StartFadeImage;
+    public bool FinishFadeOUT;
+    public bool FinishFadeIN;
     Image FadeImage;
 
 
     private void Awake()
     {
         FadeImage = this.GetComponent<Image>();
+        if (IsTimingFadeImage && !FadeIn)
+        {
+            FadeImage.enabled = false;
+        }
         if (FadeIn)
         {
             FadeImage.fillAmount = 1;
@@ -22,16 +31,22 @@ public class UI_FadeImage : MonoBehaviour
         {
             FadeImage.fillAmount = 0;
         }
+        if (SpeedUpFadeSpeedx2)
+        {
+            FadeTime = FadeTime * 2.0f;
+        }
     }
     void FixedUpdate()
     {
         FadeIN();
         FadeOUT();
+        TimingFadeIN();
+        TimingFadeOUT();
     }
 
     void FadeIN()
     {
-        if(FadeIn && !FadeOut)
+        if(FadeIn && !FadeOut && !IsTimingFadeImage)
         {
             FadeImage.fillAmount -= FadeTime;
             if (FadeImage.fillAmount <= 0.02)
@@ -43,11 +58,54 @@ public class UI_FadeImage : MonoBehaviour
 
     void FadeOUT()
     {
-        if (!FadeIn && FadeOut)
+        if (!FadeIn && FadeOut && !IsTimingFadeImage)
         {
             FadeImage.fillAmount += FadeTime;
             if(FadeImage.fillAmount >= 0.98)
             {
+            }
+        }
+    }
+
+    void TimingFadeIN()
+    {
+        if (FadeIn && !FadeOut && IsTimingFadeImage)
+        {
+            if (StartFadeImage)
+            {
+                FadeImage.enabled = true;
+                FadeImage.fillAmount -= FadeTime;
+                if (FadeImage.fillAmount <= 0.02)
+                {
+                    FadeImage.enabled = false;
+                    FinishFadeIN = true;
+                    StartFadeImage = false;
+                }
+            }
+            else if(!FinishFadeIN & !StartFadeImage)
+            {
+                FadeImage.fillAmount = 1.0f;
+            }
+        }
+    }
+
+    void TimingFadeOUT()
+    {
+        if (!FadeIn && FadeOut && IsTimingFadeImage)
+        {
+            if (StartFadeImage) 
+            {
+                FadeImage.enabled = true;
+                FadeImage.fillAmount += FadeTime;
+                if (FadeImage.fillAmount >= 0.98)
+                {
+                    FinishFadeOUT = true;
+                    StartFadeImage = false;
+                }
+            }
+            else if (!FinishFadeOUT && !StartFadeImage)
+            {
+                FadeImage.fillAmount = 0.0f;
             }
         }
     }
--- FPS_NoName/Assets/Script/UI/UI_MainMenu.cs
+++ FPS_NoName/Assets/Script/UI/UI_MainMenu.cs
@@ -5,23 +5,32 @@ using UnityEngine.UI;
 
 public class UI_MainMenu : MonoBehaviour
 {
+    UI_FadeImage FadeSystemToOption;
+    UI_FadeImage FadeSystemOptionScreen;
+
     [SerializeField] private int CurrentSelect;
 
     [SerializeField] private bool PushEsc;
     [SerializeField] private bool FadeAnimtionEnd;
+    [SerializeField] private bool CantSelectMenu;
 
     [SerializeField] private Image FadeImage;
     [SerializeField] private Image GameStartSelect_Button;
     [SerializeField] private Image OptionSelect_Button;
     [SerializeField] private Image GameStart_Button;
     [SerializeField] private Image Option_Button;
+    [SerializeField] private Image TimingFadeOutToOption;
+    [SerializeField] private Image TimingFadeInOption;
 
     [SerializeField] private GameObject MainMenuHUD;
     [SerializeField] private GameObject ExitHUD;
     [SerializeField] private GameObject OptionHUD;
+
     private void Awake()
     {
         CursorSystem();
+        FadeSystemToOption = TimingFadeOutToOption.GetComponent<UI_FadeImage>();
+        FadeSystemOptionScreen = TimingFadeInOption.GetComponent<UI_FadeImage>();
         GameStartSelect_Button.enabled = true;
         OptionSelect_Button.enabled = false;
         GameStart_Button.enabled = false;
@@ -38,9 +47,27 @@ public class UI_MainMenu : MonoBehaviour
 
     void CheckStateAnimation()
     {
+        if (FadeSystemToOption.FinishFadeOUT)
+        {
+            CantSelectMenu = false;
+            OptionHUD.SetActive(true);
+            MainMenuHUD.SetActive(false);
+            FadeSystemToOption.FinishFadeOUT = false;
+        }
+
         if (FadeImage.fillAmount <= 0.0f)
         {
             FadeAnimtionEnd = true;
+        }
+
+        if (OptionHUD.activeSelf == true && !FadeSystemOptionScreen.FinishFadeIN)
+        {
+            FadeSystemOptionScreen.StartFadeImage = true;
+        }
+        else if (FadeSystemOptionScreen.FinishFadeIN || MainMenuHUD.activeSelf == true)
+        {
+            FadeSystemOptionScreen.StartFadeImage = false;
+            FadeSystemOptionScreen.FinishFadeIN = false;
         }
     }
     void CursorSystem()
@@ -56,10 +83,8 @@ public class UI_MainMenu : MonoBehaviour
             PushEsc = !PushEsc;
         }
 
-        if (FadeAnimtionEnd && MainMenuHUD.activeSelf == true)
+        if (FadeAnimtionEnd && MainMenuHUD.activeSelf == true && (!CantSelectMenu))
         {
-            
-
             switch (CurrentSelect)
             {
                 case 0:
@@ -83,8 +108,8 @@ public class UI_MainMenu : MonoBehaviour
                     Option_Button.enabled = false;
                     if (Input.GetKeyDown(KeyCode.Space))
                     {
-                        OptionHUD.SetActive(true);
-                        MainMenuHUD.SetActive(false);
+                        CantSelectMenu = true;
+                        FadeSystemToOption.StartFadeImage = true;
                     }
                     if (Input.GetKeyDown(KeyCode.UpArrow))
                     {
@@ -97,11 +122,6 @@ public class UI_MainMenu : MonoBehaviour
                     break;
 
             }
-        }
-
-        if (OptionHUD.activeSelf == true)
-        {
-
         }
     }
 
