268fa7eedf3295fd68de01734cfe3b8d17c4b8a8
OiC-b9336-Naoki <b9336@oic.jp>
2022-01-21 11:44:42 +0900

    Character_Info内部のcurrentweaponを現在の武器と見立てて武器の切り替え(数値のみ)反映

--- FPS_NoName/Assets/Script/Character/Character_Info.cs
+++ FPS_NoName/Assets/Script/Character/Character_Info.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class Character_Info : MonoBehaviour
 {
     [SerializeField] private int character_CurrentWeapon;
+    [SerializeField] private int character_MaxWeapons;
     [SerializeField] private int character_CurrentGrenades;
     [SerializeField] private float character_CurrentHP;
     [SerializeField] private float character_MaxHP;
     [SerializeField] private float character_CurrentArmor;
     [SerializeField] private float character_MaxArmor;
-    public int Character_CurrentWeapon { get { return character_CurrentWeapon; } set { character_CurrentWeapon = value; } }
+    public int Character_CurrentWeapon
+    {
+        get { return character_CurrentWeapon; } 
+        set 
+        { 
+            character_CurrentWeapon = value;
+            if (character_CurrentWeapon < 0)
+            {
+                character_CurrentWeapon = character_MaxWeapons;
+            }
+            else if(character_CurrentWeapon > character_MaxWeapons)
+            {
+                character_CurrentWeapon = 0;
+            }
+        } 
+    }
     public int Character_CurrentGrenades { get { return character_CurrentGrenades; } set { character_CurrentGrenades = value; } }
 
     public float Character_CurrentHP { get { return character_CurrentHP; } set { character_CurrentHP = value; } }
--- FPS_NoName/Assets/Script/Character/Character_State.cs
+++ FPS_NoName/Assets/Script/Character/Character_State.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Character_State : MonoBehaviour
 {
-    [SerializeField]Weapon_State weapon;
+    //•¡”•Ší‚Ö‚Ì‘Î‰ž‚Ì‚½‚ßƒŠƒXƒg‰»‚·‚é—\’è
+    [SerializeField] Weapon_State weapon_state;
+    [SerializeField] Character_Info character_Info;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +19,21 @@ public class Character_State : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-           weapon.Reload();
+            weapon_state.Reload();
         }
 
         if (Input.GetButton("Fire1"))
         {
-           weapon.Shot();
+            weapon_state.Shot();
+        }
+        float wheelroll = Input.GetAxis("Mouse ScrollWheel");
+        if(wheelroll > 0)
+        {
+            character_Info.Character_CurrentWeapon--;
+        }
+        else if(wheelroll < 0)
+        {
+            character_Info.Character_CurrentWeapon++;
         }
     }
 }
