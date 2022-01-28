bda2d82b1685bd9216bb5a479af0720fe296e81d
DESKTOP-G23FMHQ\senz <ryuuki285666@gmail.com>
2022-01-28 13:14:21 +0900

    メインメニューのexitからゲームが終了できるように実装 / maingameマップに敵を配置

--- FPS_NoName/Assets/Script/UI/UI_MainMenu.cs
+++ FPS_NoName/Assets/Script/UI/UI_MainMenu.cs
@@ -13,6 +13,11 @@ public class UI_MainMenu : MonoBehaviour
     UI_FadeImage FadeSystemFadeOutMainMenu;                 //メインメニューのフェードアウト用の画像
 
     [SerializeField] private int CurrentSelect;             //メインメニューの現在選んでいる項目用のint
+    [SerializeField] private int CurrentSelectExit;         //終了メニューの現在選んでる項目用のint
+    [SerializeField] private Image Exit_SelectYes;
+    [SerializeField] private Image Exit_SelectNo;
+    [SerializeField] private Image Exit_Yes;
+    [SerializeField] private Image Exit_No;
     [SerializeField] private bool PushEsc;                  //ESCキーが押されているか、いないかの確認用のbool
     [SerializeField] private bool FadeAnimtionEnd;          //開幕のフェードアニメーションの終了確認用 bool
     public bool CantSelectMenu;                             //フェード中や、何かを実行しているときにメインメニューを触らせないようにするbool
@@ -132,8 +137,8 @@ public class UI_MainMenu : MonoBehaviour
     void InputDir()
     {
         //フェードアニメーション実行中は、ESCキーを実行させない
-        if (FadeImage.fillAmount <= 0.1f && !FadeSystemFadeInOption.StartFadeImage && !FadeSystemFadeOutMainMenu.StartFadeImage 
-            && !FadeSystemOptionScreen.StartFadeImage && !FadeSystemToOption.StartFadeImage && !OptionSystem.isApplyShown) 
+        if (FadeImage.fillAmount <= 0.1f && !FadeSystemFadeInOption.StartFadeImage && !FadeSystemFadeOutMainMenu.StartFadeImage
+            && !FadeSystemOptionScreen.StartFadeImage && !FadeSystemToOption.StartFadeImage && !OptionSystem.isApplyShown)
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
@@ -193,6 +198,50 @@ public class UI_MainMenu : MonoBehaviour
                     break;
 
             }
+        }
+
+        if (ExitHUD.activeSelf)
+        {
+            switch (CurrentSelectExit)
+            {
+                case 0:
+                    Exit_SelectNo.enabled = false;
+                    Exit_Yes.enabled = false;
+                    Exit_SelectYes.enabled = true;
+                    Exit_No.enabled = true;
+                    if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+                    {
+                        CurrentSelectExit = 1;
+                    }
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        #if UNITY_EDITOR
+                        UnityEditor.EditorApplication.isPlaying = false;
+                        #endif
+                        #if UNITY_STANDALONE
+                        Application.Quit();
+                        #endif
+                    }
+                    break;
+                case 1:
+                    Exit_SelectNo.enabled = true;
+                    Exit_Yes.enabled = true;
+                    Exit_SelectYes.enabled = false;
+                    Exit_No.enabled = false;
+                    if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+                    {
+                        CurrentSelectExit = 0;
+                    }
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        MainMenuHUD.SetActive(true);
+                        PushEsc = false;
+                        ExitHUD.SetActive(false);
+                    }
+                    break;
+
+            }
+
         }
     }
 
