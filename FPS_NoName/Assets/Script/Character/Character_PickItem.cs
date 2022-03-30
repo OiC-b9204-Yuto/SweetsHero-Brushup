c5b6a6c674d265f0d34eb23159d9bc142ed585d4
OiC_Watanabe <B9473@oic.jp>
2022-03-30 17:17:49 +0900

    アイテムの獲得モードを変更時に消し忘れた無駄な物を削除

--- FPS_NoName/Assets/Script/Character/Character_PickItem.cs
+++ FPS_NoName/Assets/Script/Character/Character_PickItem.cs
@@ -4,17 +4,10 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Character_PickItem : MonoBehaviour
-{
-    public enum ItemPickMode //アイテムピックモード
-    {
-        AutoPick,
-        ManualPick,
-    }
-    
+{   
     private int ScreenCenterX; //画面の中央 X 座標を取得
     private int ScreenCenterY; //画面の中央 Y 座標を取得
     private Vector3 CenterPos; //画面の中央のベクトルを取得
-    public ItemPickMode itemPickMode; //アイテムピックモードの参照
     [SerializeField] private Camera PlayerCamera; //プレイヤーカメラ
     [SerializeField] private float ManualPickDistance; //マニュアルピックのアイテムを取得できる距離
     private Ray ManualItem_Ray;  //アイテムにレイを飛ばす
@@ -23,7 +16,6 @@ public class Character_PickItem : MonoBehaviour
     public bool IsHitItem;
     private void Awake()
     {
-        itemPickMode = ItemPickMode.ManualPick;
         ScreenCenterX = Screen.width / 2;
         ScreenCenterY = Screen.height / 2;
         CenterPos = new Vector3(ScreenCenterX, ScreenCenterY, 1.0f);
