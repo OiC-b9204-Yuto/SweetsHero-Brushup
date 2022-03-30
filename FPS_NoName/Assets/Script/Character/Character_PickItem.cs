c9d5e19cb437d60a693dfb38d144cf1e0426c41d
OiC_Watanabe <B9473@oic.jp>
2022-03-30 17:15:13 +0900

    アイテムの獲得モードを削除 --> マニュアルのみに変更 それに伴い GameUIの内容を変更

--- FPS_NoName/Assets/Script/Character/Character_PickItem.cs
+++ FPS_NoName/Assets/Script/Character/Character_PickItem.cs
@@ -17,11 +17,8 @@ public class Character_PickItem : MonoBehaviour
     public ItemPickMode itemPickMode; //アイテムピックモードの参照
     [SerializeField] private Camera PlayerCamera; //プレイヤーカメラ
     [SerializeField] private float ManualPickDistance; //マニュアルピックのアイテムを取得できる距離
-    [SerializeField] private SphereCollider AutoPickArea; //自動ピックエリア
-    private float changeCoolDown; //アイテムモード切り替えのクールタイム
     private Ray ManualItem_Ray;  //アイテムにレイを飛ばす
     private RaycastHit manualItem_Hit; //レイにアイテムが当たっている情報
-    public float ChangeCoolDown { get { return changeCoolDown; } } //クールタイム取得用
     public RaycastHit ManualItem_Hit { get { return manualItem_Hit; } } //アイテム情報取得用
     public bool IsHitItem;
     private void Awake()
@@ -34,66 +31,27 @@ public class Character_PickItem : MonoBehaviour
 
     void Update()
     {
-        ModeChange();
         PickSystem();
     }
 
-    void ModeChange()
-    {
-        if (changeCoolDown > 0.0f)
-        {
-            changeCoolDown -= Time.deltaTime;
-            return;
-        }
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            switch (itemPickMode)
-            {
-                case ItemPickMode.AutoPick:
-                    itemPickMode = ItemPickMode.ManualPick;
-                    changeCoolDown = 5.0f;
-                    break;
-                case ItemPickMode.ManualPick:
-                    itemPickMode = ItemPickMode.AutoPick;
-                    changeCoolDown = 5.0f;
-                    break;
-            }
-        }
-    }
 
     void PickSystem()
     {
-        switch (itemPickMode)
+        ManualItem_Ray = PlayerCamera.ScreenPointToRay(CenterPos);
+        Debug.DrawRay(ManualItem_Ray.origin, ManualItem_Ray.direction * ManualPickDistance, Color.red, 0.5f, false);
+        LayerMask layerMask = 1;
+        if (Physics.Raycast(ManualItem_Ray, out manualItem_Hit, ManualPickDistance, layerMask) && ManualItem_Hit.collider.tag == "Item")
         {
-            case ItemPickMode.AutoPick: //自動ピックの場合
-                AutoPickArea.enabled = true;
-                break;
-            case ItemPickMode.ManualPick: //手動ピックの場合
-                ManualItem_Ray = PlayerCamera.ScreenPointToRay(CenterPos);
-                Debug.DrawRay(ManualItem_Ray.origin, ManualItem_Ray.direction * ManualPickDistance, Color.red, 0.5f, false);
-                AutoPickArea.enabled = false;
-                LayerMask layerMask = 1;
-                if (Physics.Raycast(ManualItem_Ray, out manualItem_Hit, ManualPickDistance, layerMask) && ManualItem_Hit.collider.tag == "Item")
-                {
-                    IsHitItem = true;
-                    if (Input.GetKeyDown(KeyCode.E))
-                    {
-                        manualItem_Hit.collider.GetComponent<DropItem_Setting>().GetItem();
-                    }
-                }
-                else
-                {
-                    IsHitItem = false;
-                }
-                break;
+            IsHitItem = true;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                manualItem_Hit.collider.GetComponent<DropItem_Setting>().GetItem();
+            }
         }
-    }
-
-    private void OnTriggerEnter(Collider col)
-    {
-        if(col.gameObject.tag == "Item")
+        else
         {
-            col.GetComponent<DropItem_Setting>().GetItem();
+            IsHitItem = false;
         }
     }
 }
+
